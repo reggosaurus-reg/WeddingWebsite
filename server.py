6fe0640d006f0f220eae8364a93c4a46cd47f23f
Regina Hansson <regha434@student.liu.se>
2020-02-12 09:01:30 +0100

    File structure and server routes all paths

--- server.py
+++ server.py
@@ -4,8 +4,27 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/valkomna')
 def welcome_page():
-    return "HEllo worLD"
-    #return render_template("index.html")
+    return render_template("index.html")
+
+@app.route('/viktiginformation')
+def info_page():
+    return render_template("viktiginformation.html")
+
+@app.route('/vardar')
+def tm_page():
+    return render_template("vardar.html")
+
+@app.route('/onskelista')
+def wishlist_page():
+    return render_template("onskelista.html")
+
+@app.route('/boende')
+def hotels_page():
+    return render_template("boende.html")
+
+@app.route('/anmalan')
+def general_page():
+    return render_template("anmalan.html")
 
 if __name__ == '__main__':
     app.run()
