07e1b1418ca0a1adf9fe191da6b968b1df7c52c4
Regina Hansson <regha434@student.liu.se>
2020-02-12 14:44:50 +0100

    Small try on input in forms

--- static/js/password.js
+++ static/js/password.js
@@ -2,8 +2,13 @@ window.onload = () => {
 	var password = prompt("Hur många ben har en fisk?");
 
 	while (password != "42 kanske") {
+		if (password == null) {
+			return;
+		}
 		console.log("Fel! " + password);
 		password = prompt("Nej. Hur många ben har en fisk?");
 	}
 	console.log("Rätt!");
+	document.getElementById("secretlist").style.display = "block";
+	document.getElementById("unsecret").style.display = "none";
 };
