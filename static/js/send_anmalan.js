86f5f121e606f3ec2e9cbc68c45402d3eace9ec5
Regina Hansson <regha434@student.liu.se>
2020-03-16 21:26:06 +0100

    Start adding values to json, but checkbox values need to be set

--- static/js/send_anmalan.js
+++ static/js/send_anmalan.js
@@ -11,7 +11,15 @@ window.addEventListener("load",function(event) {
 		var jsonobj = JSON.stringify({
 			name: document.getElementById("name").value,
 			email: document.getElementById("email").value,
+			gluten: document.getElementById("gluten").value,
+			laktos: document.getElementById("laktos").value,
+			vegetarian: document.getElementById("vegetarian").value,
+			vegan: document.getElementById("vegan").value,
+			allergy_check: document.getElementById("allergy_check").value,
+			allergy: document.getElementById("allergy").value,
+			other: document.getElementById("other").value,
 			time: new Date()});
+		alert(jsonobj);
 		xhttp.send(jsonobj);
 	};
 });
