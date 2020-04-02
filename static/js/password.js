2c95191966fd7eb29050eb8615185c74dff33020
Regina Hansson <regha434@student.liu.se>
2020-04-02 14:41:07 +0200

    Put back password on list page

--- static/js/password.js
+++ static/js/password.js
@@ -1,17 +1,15 @@
 window.onload = () => {
-	/* DEBUG
-	var password = prompt("Hur många ben har en fisk?");
+	var question = "Vad kommer man tänka när man ser bruden?";
+	var password = prompt(question);
 
 	// TODO: This is not secure. Noones email is secure anywhere.
-	while (password != "42 kanske") {
+	while (password != "paMpigt") {
 		if (password == null) { // When does this happen?
 			return;
 		}
 		console.log("Fel lösen! " + password);
-		password = prompt("Nej. Hur många ben har en fisk?");
+		password = prompt("Nej. " +  question);
 	}
-	*/
-	//document.getElementById("people_list").textContent = "Tets";
 	document.getElementById("secretlist").style.display = "block";
 	document.getElementById("unsecret").style.display = "none";
 };
