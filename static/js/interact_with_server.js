9b58da523d82b27a33cb355bca2b449e4c73d807
Regina Hansson <regha434@student.liu.se>
2020-04-11 18:43:16 +0200

    Move signup js to the one file

--- static/js/interact_with_server.js
+++ static/js/interact_with_server.js
@@ -1,6 +1,27 @@
 window.onload = () => {
 	switch (PAGE_TYPE) {
 		case "signup":
+			showIfNotPastDeadline();
+			document.getElementById("signup").onclick = (e) => {
+				// Timeout
+				setMessage("anmalan_status", "Försöker skicka din anmälan...");
+				setTimeout(function() {
+					setMessage("anmalan_status",
+					"Anmälan kunde inte skickas. Försök igen!");
+				}, 5000);
+
+				// Pack and send field data
+				var data = {
+					name: 		document.getElementById("name").value,
+					email: 		document.getElementById("email").value,
+					gluten: 	document.getElementById("gluten").checked,
+					laktos: 	document.getElementById("laktos").checked,
+					vegetarian: document.getElementById("vegetarian").checked,
+					vegan: 		document.getElementById("vegan").checked,
+					allergy: 	document.getElementById("allergy").value,
+					other: 		document.getElementById("other").value};
+				sendPost(readyShowS, "anmalan", data);
+			};
 			break;
 		case "wishlist":
 			document.getElementById("reserve").onclick = (e) => {
@@ -8,12 +29,47 @@ window.onload = () => {
 			}
 			break;
 		case "admin_signup":
+			let passwordS = prompt("Vad heter Reginas katt?");
+			sendPost(readyShowSA, { enter_password: passwordS });
+			// REMOVE ENTRIES
+			document.getElementById("remove").onclick = (e) => {
+				function readyRemove() {
+					if (this.readyState == 4) {
+						alert(this.responseText);
+						if (this.status == 200) {
+							sendGet(readyReloadWA);
+						}
+					}
+				}
+
+				let to_remove = getNamesToRemove();
+				if (to_remove.length == 0) {
+					alert("Du har inte valt någon att ta bort.");
+				}
+				else if (confirm("Är du säker på att du vill ta bort " + to_remove.length +
+					" person(er) från databasen?")) {
+					let sure = prompt("Helt säker?");
+					sendPost(readyRemove, { remove: to_remove, remove_password: sure });
+				}
+			};
+
+			// GET CSV
+			document.getElementById("download").onclick = (e) => {
+				function readyCsv() {
+					if (this.readyState == 4 && this.status == 200) {
+						downloadCsvFile(this.responseText);
+					}
+				};
+				sendPost(readyCsv, { fetch_csv: true });
+			};
 			break;
 		case "admin_wishlist":
-			let password = prompt("Vad heter Reginas katt?");
-			sendPost(readyShow, "onskelistaadmin", { enter_password: password });
+			let passwordW = prompt("Vad heter Reginas katt?");
+			sendPost(readyShowW, "onskelistaadmin", { enter_password: passwordW });
 			break;
+		// TODO: Put homepage here
 		default:
+			// TODO: Put menu here
 			console.log(PAGE_TYPE + " is an invalid page type.");
 	}
 }
@@ -35,7 +91,8 @@ function sendGet(readyFn, url) {
 	xhttp.send();
 }
 
-// ---- WISHLIST
+// ---- CALLS
+
 
 function readyUpdateW() {
 	if (this.readyState == 4) {
@@ -49,6 +106,14 @@ function readyUpdateW() {
 	}
 }
 
+// TODO: Thsís is never called...
+function readyReloadS() {
+	if (this.readyState == 4 && this.status == 200) {
+		document.body.innerHTML = this.responseText;
+		showPageContent()
+	}
+}
+
 function readyReloadW() {
 	if (this.readyState == 4 && this.status == 200) {
 		document.body.innerHTML = this.responseText;
@@ -57,6 +122,53 @@ function readyReloadW() {
 		}
 	}
 }
+
+function readyShowS() {
+	if (this.readyState == 4 && this.status == 200) {
+		removeErrors();
+		clearFilledData();
+		setMessage("anmalan_status", this.responseText);
+	}
+	else if (this.readyState == 4 && this.status == 418) {
+		setErrors(JSON.parse(this.responseText));
+	}
+};
+
+function readyShowSA() {
+	if (this.readyState == 4) {
+		switch(this.status) {
+			case 200: showPageContent(); break;
+			case 401: hidePageContent(); break;
+		}
+	}
+}
+
+function readyReloadWA() {
+	if (this.readyState == 4 && this.status == 200) {
+		document.body.innerHTML = this.responseText;
+		setupPageContent()
+	}
+}
+
+function readyUpdateWA() {
+	if (this.readyState == 4) {
+		let e = document.getElementById("modify_error");
+		e.textContent = this.responseText;
+		e.style.display = 'block';
+		if (this.status == 200) {
+			sendGet(readyReloadWA, "onskelistaadmin");
+		}
+	}
+}
+function readyShowW() {
+	if (this.readyState == 4) {
+		switch(this.status) {
+			case 200: setupPageContent(); break;
+			case 401: hidePageContent(); break;
+		}
+	}
+}
+// ---- WISHLIST
 
 function getItemsToReserve() {
 	let items = document.querySelectorAll(".reserve_name");
@@ -80,31 +192,6 @@ function getItemsToReserve() {
 
 // ---- WISHLIST ADMIN
 
-function readyReloadWA() {
-	if (this.readyState == 4 && this.status == 200) {
-		document.body.innerHTML = this.responseText;
-		setupPageContent()
-	}
-}
-
-function readyUpdateWA() {
-	if (this.readyState == 4) {
-		let e = document.getElementById("modify_error");
-		e.textContent = this.responseText;
-		e.style.display = 'block';
-		if (this.status == 200) {
-			sendGet(readyReloadWA, "onskelistaadmin");
-		}
-	}
-}
-function readyShow() {
-	if (this.readyState == 4) {
-		switch(this.status) {
-			case 200: setupPageContent(); break;
-			case 401: hidePageContent(); break;
-		}
-	}
-}
 
 function removeFunction() {
 	let to_remove = getItemsToRemove();
@@ -182,4 +269,108 @@ function getItemsToRemove() {
 		}
 	}
 	return to_remove;
+}
+
+// ---- SIGNUP ADMIN
+
+function showPageContent() {
+	// Pretty print booleans and calculate summary
+	let totals = {"g": 0, "l": 0, "vt": 0, "vg": 0}
+	let bools = document.querySelectorAll(".bool");
+	for (let i = 0; i < bools.length; i++) {
+		let b = parseInt(bools[i].innerHTML);
+		let type = bools[i].classList[2];
+		if (b) {
+			bools[i].innerHTML = "x";
+			totals[type] += 1;
+		} else {
+			bools[i].innerHTML = "-";
+		}
+	}
+
+	// Print summary
+	for (let type in totals) {
+		document.querySelector("#total_" + type).innerHTML = totals[type];
+	}
+
+	// Show table
+	document.getElementById("secretlist").style.display = "block";
+	document.getElementById("unsecret").style.display = "none";
+}
+
+
+function getNamesToRemove() {
+	let names = document.querySelectorAll(".people_name");
+	let checkboxes = document.querySelectorAll(".people_remove input");
+	// Filter out entries to remove
+	let to_remove = [];
+	console.assert(names.length == checkboxes.length,
+		"Name and box lengths differ. Something's wrong.");
+	for (let i = 0; i < names.length; i++) {
+		let name = names[i].innerHTML;
+		let remove = checkboxes[i].checked;
+		if (remove) {
+			to_remove.push(name)
+		}
+	}
+	return to_remove;
+}
+
+
+function downloadCsvFile(content) {
+	let preamble = "data:text/csv;charset=utf-8,";
+	var encodedUri = encodeURI(preamble + content);
+	var link = document.createElement("a");
+	link.setAttribute("href", encodedUri);
+	link.setAttribute("download", "wedding_guests.csv");
+	document.body.appendChild(link);
+	link.click();
+}
+
+// ---- SIGNUP
+function showIfNotPastDeadline() {
+	let deadline = new Date("Jun 2, 2020 00:00:00").getTime();
+	let now = new Date().getTime();
+	if (deadline - now < 0) {
+		document.getElementById("too_late").style.display = "block";
+		document.getElementById("time_left").style.display = "none";
+	} else {
+		document.getElementById("too_late").style.display = "none";
+		document.getElementById("time_left").style.display = "block";
+	}
+}
+
+function removeErrors() {
+	document.getElementById("name_error").style.display = 'none';
+	document.getElementById("email_error").style.display = 'none';
+	document.getElementById("info_error").style.display = 'none';
+	document.getElementById("allergy_error").style.display = 'none';
+}
+
+function clearFilledData() {
+	document.getElementById("name").value = "";
+	document.getElementById("email").value = "";
+	document.getElementById("gluten").checked = false;
+	document.getElementById("laktos").checked = false;
+	document.getElementById("vegetarian").checked = false;
+	document.getElementById("vegan").checked = false;
+	document.getElementById("allergy").value = "";
+	document.getElementById("other").value = "";
+}
+
+function setErrors(faulty) {
+	setMessage("name_error", faulty.name);
+	setMessage("email_error", faulty.email);
+	setMessage("info_error", faulty.info);
+	setMessage("allergy_error", faulty.allergy);
+	setMessage("anmalan_status", "Ett eller flera fält är felaktigt ifyllt.");
+}
+
+function setMessage(elementId, fieldContent) {
+	// If there is a message to set
+	if (fieldContent != "undefined") {
+		let e = document.getElementById(elementId);
+		e.textContent = fieldContent;
+		e.style.display = 'block';
+	}
 }
